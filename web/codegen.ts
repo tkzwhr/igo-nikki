69fbf71f17c817103e3a4587d9b7642c3d9b3936
Hiroki Takizawa <tkzwhr@gmail.com>
2024-07-14 11:28:14 +0900

    fix

--- web/codegen.ts
+++ web/codegen.ts
@@ -1,15 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: [
-    {
-      "http://localhost:20180/v1/graphql": {
-        headers: {
-          "x-hasura-admin-secret": "hasura",
-        },
-      },
-    },
-  ],
+  schema: "../graphql/schema.graphql",
   documents: ["src/queries/*.ts"],
   generates: {
     "./src/generated/gql/": {
